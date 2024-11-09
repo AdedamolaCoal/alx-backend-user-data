da71066591dc9c720eceff679003dabc6181114e
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-11-09 12:16:13 +0100

    task 2 done

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -4,9 +4,12 @@ This module contains functions for obfuscating sensitive data in log messages.
 """
 
 import re
+import logging
 from typing import List
 
-import logging
+
+
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
 
 
 def replace_with_redaction(redaction: str):
@@ -69,4 +72,21 @@ class RedactingFormatter(logging.Formatter):
         """
         record.msg = filter_datum(self.fields, self.REDACTION, record.msg, self.SEPARATOR)
         return super().format(record)
-      
+
+def get_logger() -> logging.Logger:
+    """
+    Creates and configures a logger for user data with sensitive fields redacted.
+
+    Returns:
+        logging.Logger: Configured logger instance for user data.
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    handler = logging.StreamHandler()
+    handler.setFormatter(RedactingFormatter(fields=PII_FIELDS))
+    logger.addHandler(handler)
+
+    return logger
+  
