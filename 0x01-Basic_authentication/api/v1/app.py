13a4c0501bc3926f539956ec2df21a31f6bc93a7
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-11-12 14:14:57 +0100

    task 6 done

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -15,7 +15,10 @@ auth = None
 
 # Set up the authentication based on AUTH_TYPE environment variable
 AUTH_TYPE = os.getenv("AUTH_TYPE")
-if AUTH_TYPE == "auth":
+if AUTH_TYPE == "basic_auth":
+    from api.v1.auth.basic_auth import BasicAuth
+    auth = BasicAuth()
+elif AUTH_TYPE == "auth":
     from api.v1.auth.auth import Auth
     auth = Auth()
 
