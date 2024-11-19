fbdd07d329d975eb2d5e24da46a54a687b100f7a
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-11-20 00:42:24 +0100

    done

--- 0x03-user_authentication_service/db.py
+++ 0x03-user_authentication_service/db.py
@@ -15,7 +15,8 @@ class DB:
 
     def __init__(self) -> None:
         """Initialize a new DB instance."""
-        self._engine = create_engine("sqlite:///a.db", echo=True)
+        self._engine = create_engine(
+            "sqlite:///a.db", echo=False)  # Turn off verbose logging
         Base.metadata.drop_all(self._engine)
         Base.metadata.create_all(self._engine)
         self.__session = None
