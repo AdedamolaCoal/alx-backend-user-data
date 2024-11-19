50c4ab69a1adffe8afbf91e035fb5b9990601a0c
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-11-19 23:53:42 +0100

    update

--- 0x02-Session_authentication/api/v1/views/users.py
+++ 0x02-Session_authentication/api/v1/views/users.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 """
-user module for managing user routes.
+View module for handling user-related operations.
+
+This module defines routes for retrieving
+user information, including
+the authenticated user and users by ID.
 """
 
 from flask import jsonify, abort, request
@@ -11,13 +15,20 @@ from api.v1.views import app_views
 @app_views.route('/users/<user_id>', methods=['GET'], strict_slashes=False)
 def get_user(user_id):
     """
-    Retrieve a user by ID or the authenticated user (me).
+    Retrieve a user by their ID or
+    the authenticated user (me).
 
     Args:
-        user_id: id of user
+        user_id (str):
+                The ID of the user to retrieve. If the ID is "me",
+                it refers to the currently authenticated user.
 
-    Return:
-        User or 404
+    Returns:
+        Response: A JSON response containing the user data.
+
+    Raises:
+        404: If the user ID does not exist or
+        the user is not authenticated.
     """
     if user_id == "me":
         if request.current_user is None:
