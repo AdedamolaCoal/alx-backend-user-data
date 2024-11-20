b8bb5cf6f313f4c5c65cab7ecfdf60a7131aa4f1
AdedamolaCoal <117821207+AdedamolaCoal@users.noreply.github.com>
2024-11-20 01:00:59 +0100

    task 7 done

---
+++ 0x03-user_authentication_service/app.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""
+Basic Flask app.
+"""
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+
+@app.route("/", methods=["GET"])
+def home():
+    """
+    GET route at "/" that returns a JSON response.
+
+    Returns:
+        A JSON payload with a welcome message.
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5001")
